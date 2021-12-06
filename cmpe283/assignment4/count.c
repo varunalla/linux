addf90a64c88655798067a9cdaa5ff16d12668a2
Varun Alla <varun.alla@gmail.com>
2021-12-05 22:32:13 -0800

    created count.c
    code to be run inside the nested vm, will call the cpuid

---
+++ cmpe283/assignment4/count.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void totalExitCountWithReason(int reason)
+{
+    unsigned a, b, c, d;
+    unsigned dreason = reason;
+    asm("mov %0,%%ecx \n\t" ::"r"(dreason));
+    asm("mov $0x4ffffffd,%eax \n\t");
+    asm("cpuid\n");
+    asm("mov %%eax, %0\n\t"
+        : "=r"(a));
+    asm("mov %%ebx, %0\n\t"
+        : "=r"(b));
+    asm("mov %%ecx, %0\n\t"
+        : "=r"(c));
+    asm("mov %%edx, %0\n\t"
+        : "=r"(d));
+    printf("CPUID(0x4ffffffd) ");
+    if (a == 0 && b == 0 && c == 0 && d == 0)
+    {
+        a = 0;
+    }
+    else if (a == 0 && b == 0 && c == 0 && d == 0xffffffff)
+    {
+        b = 0;
+    }
+    else
+    {
+        printf("%d:%d \n", dreason, a);
+    }
+}
+
+void totalExitTimeWithReason(int reason)
+{
+    unsigned a, b, c, d;
+    uint32_t ebx, ecx;
+    uint64_t time;
+    unsigned dreason = reason;
+
+    asm("mov %0,%%ecx \n\t" ::"r"(dreason));
+    asm("mov $0x4ffffffc,%eax \n\t");
+    asm("cpuid\n");
+    asm("mov %%eax, %0\n\t"
+        : "=r"(a));
+    asm("mov %%ebx, %0\n\t"
+        : "=r"(b));
+    asm("mov %%ecx, %0\n\t"
+        : "=r"(c));
+    asm("mov %%edx, %0\n\t"
+        : "=r"(d));
+    ebx = (uint32_t)b;
+    ecx = (uint32_t)c;
+    time = (uint64_t)ebx << 32 | ecx;
+    printf("CPUID(0x4ffffffc) ");
+    if (a == 0 && b == 0 && c == 0 && d == 0)
+    {
+        a = 0;
+    }
+    else if (a == 0 && b == 0 && c == 0 && d == 0xffffffff)
+    {
+        b = 0;
+    }
+    else
+    {
+        printf("%d:%ld\n", dreason, time);
+    }
+}
+void getCpuIDTimewithReason(int reason)
+{
+    asm("xor %eax , %eax\n\t");
+    asm("xor %ebx , %ebx\n\t");
+    asm("xor %ecx , %ecx\n\t");
+    asm("xor %edx , %edx\n\t");
+    totalExitTimeWithReason(reason);
+}
+void getCpuIDCountwithReason(int reason)
+{
+    asm("xor %eax , %eax\n\t");
+    asm("xor %ebx , %ebx\n\t");
+    asm("xor %ecx , %ecx\n\t");
+    asm("xor %edx , %edx\n\t");
+    totalExitCountWithReason(reason);
+}
+void totalExitCount()
+{
+    int b;
+    asm("mov $0x4fffffff,%eax \n\t");
+    asm("cpuid\n");
+    asm("mov %%eax, %0\n\t"
+        : "=r"(b));
+    printf("%d\n", b);
+}
+void getTotalTime()
+{
+    unsigned b, c;
+    uint32_t ebx, ecx;
+    uint64_t time;
+    asm("mov $0x4FFFFFFE,%eax \n\t");
+    asm("cpuid\n");
+    asm("mov %%ebx, %0\n\t"
+        : "=r"(b));
+    asm("mov %%ecx, %0\n\t"
+        : "=r"(c));
+    ebx = (uint32_t)b;
+    ecx = (uint32_t)c;
+    time = (uint64_t)ebx << 32 | ecx;
+    printf("%ld\n", time);
+}
+void getCpuID()
+{
+    asm("xor %eax , %eax\n\t");
+    asm("xor %ebx , %ebx\n\t");
+    asm("xor %ecx , %ecx\n\t");
+    asm("xor %edx , %edx\n\t");
+    printf("CPUID(0x4fffffff) ");
+    printf("total exit count is ");
+
+    totalExitCount();
+    asm("xor %eax , %eax\n\t");
+    asm("xor %ebx , %ebx\n\t");
+    asm("xor %ecx , %ecx\n\t");
+    asm("xor %edx , %edx\n\t");
+    printf("CPUID(0x4ffffffe) ");
+    printf("total cpu cycles spent in vmexits  ");
+    getTotalTime();
+    printf("\n");
+}
+int main(int argc, char *argv[])
+{
+    int reason = 0;
+    for (; reason < 75; reason++)
+    {
+        getCpuIDCountwithReason(reason);
+    }
+}
